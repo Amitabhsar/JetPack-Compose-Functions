4cf2ff32ef8375b24735132fa3a313bd76e451c6
Amitabhsar <148541062+Amitabhsar@users.noreply.github.com>
2024-01-21 19:55:36 +0300

    Add files via upload

---
+++ MessageData.kt
@@ -0,0 +1,19 @@
+package com.example.composetutorial
+
+object MessageData {
+    val MessageList = listOf(
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features."),
+        Message(sender = "Sita", body = "JetPack Compose Features are all about excitements. Do you have experience of working with the functions?"),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. I am so excited about JetPack Compose Features. I am loving it. It is full of fun."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. It is so cool."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. I am so excited about JetPack Compose Features."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. What about you Ram?"),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. I am so excited about JetPack Compose Features. Explore the functions now."),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features"),
+        Message(sender = "Sita", body = "I am so excited about JetPack Compose Features. I am so excited about JetPack Compose Features."),
+
+        )
+}
